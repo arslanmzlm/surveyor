a71f9207f4eaf75d8e240fbda11471b02c944111
Mazlum Arslan <arslan.mzlm@gmail.com>
2023-05-18 21:08:53 +0300

    added login

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AttemptLoginRequest;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param \App\Http\Requests\LoginRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate(LoginRequest $request): \Illuminate\Http\Response
+    {
+        $credentials = $request->validated();
+
+        if (Auth::attempt($credentials)) {
+            $token = $request->user()->createToken('LOGIN');
+
+            return response(['user' => $request->user(), 'token' => $token->plainTextToken]);
+        }
+
+        return response(['message' => trans('auth.failed')], 422);
+    }
+}
