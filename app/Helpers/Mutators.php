a20a84772913471249a48132c2e45530a2fc934b
Mazlum Arslan <arslan.mzlm@gmail.com>
2023-05-20 22:18:37 +0300

    added mutator helper

---
+++ app/Helpers/Mutators.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Helpers;
+
+class Mutators
+{
+    /**
+     * Mutate telephone numbers to clean type
+     *
+     * @param string|null $str
+     *
+     * @return string|null
+     */
+    public static function cleanPhone(?string $str): ?string
+    {
+        if (strlen($str) >= 10) {
+            $data = preg_replace('/\D/', '', $str);
+            return preg_replace('/[90|0]*(0[1-9][0-9]{9})/', '$1', $data);
+        }
+
+        return $str;
+    }
+}
