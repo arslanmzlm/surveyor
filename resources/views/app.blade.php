5753c9b19a12efefeee00fe867703baa92cf04e9
Mazlum Arslan <arslan.mzlm@gmail.com>
2023-07-21 00:29:30 +0300

    updated demo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,8 +10,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
-    <script defer="defer" src="/js/chunk-vendors.f7f503aa.js"></script>
-    <script defer="defer" src="/js/app.9ca12e0a.js"></script>
+    <script defer="defer" src="/js/chunk-vendors.cd8cbf69.js"></script>
+    <script defer="defer" src="/js/app.3eb3895a.js"></script>
     <link href="/css/chunk-vendors.a7e572c3.css" rel="stylesheet">
     <link href="/css/app.28fd473d.css" rel="stylesheet">
 </head>
