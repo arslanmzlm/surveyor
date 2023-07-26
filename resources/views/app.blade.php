2497a9144a9a23bf8113856b0188a8d97e1c323f
Mazlum Arslan <arslan.mzlm@gmail.com>
2023-07-26 04:26:46 +0300

    updated demo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,10 +10,10 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
-    <script defer="defer" src="/js/chunk-vendors.cd8cbf69.js"></script>
-    <script defer="defer" src="/js/app.088d74f1.js"></script>
-    <link href="/css/chunk-vendors.a7e572c3.css" rel="stylesheet">
-    <link href="/css/app.28fd473d.css" rel="stylesheet">
+    <script defer="defer" src="/assets/js/chunk-vendors.cd8cbf69.js"></script>
+    <script defer="defer" src="/assets/js/app.39758012.js"></script>
+    <link href="/assets/css/chunk-vendors.04cae51b.css" rel="stylesheet">
+    <link href="/assets/css/app.173aeafa.css" rel="stylesheet">
 </head>
 
 <body><noscript><strong>We're sorry but surveyor-front doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
