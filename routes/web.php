0d3f93aafaf50c98587341c90364bf392b2384e6
Mazlum Arslan <arslan.mzlm@gmail.com>
2023-07-26 04:46:04 +0300

    fix demo

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/link-storage', function () {
-    Artisan::call('storage:link');
-});
-
 Route::any('{query}', function () {
     return view('app');
 })->where('query', '.*');
